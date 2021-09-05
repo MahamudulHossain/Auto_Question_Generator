64c55e0769337450804cc7cc0ca22047a16ba1ea
MahamudulHossain <md.mahamudulhossainn@gmail.com>
2021-09-05 15:42:14 +0600

    Semester Section Completed

---
+++ admin/editSemData.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+include('../database.inc.php');
+include('../function.inc.php');
+include('../constant.inc.php');
+
+	if(isset($_POST['editID']) && $_POST['editID']>0){
+	    $editID=get_safe_value($_POST['editID']);
+	    $res = mysqli_query($con,"select * from semesters where id='$editID' ");
+	    if(mysqli_num_rows($res) > 0){
+	    	$row = mysqli_fetch_assoc($res);
+	    	echo json_encode($row);
+	    }
+	}
+
+?>
--- admin/semester.php
+++ admin/semester.php
@@ -1,4 +1,23 @@
-<?php include('top.php'); ?>
+<?php 
+include('top.php'); 
+
+if(isset($_GET['did']) && $_GET['did']>0){
+    $did=get_safe_value($_GET['did']);
+    mysqli_query($con,"Delete from semesters where id='$did'");
+    redirect('semester.php');
+}
+
+if(isset($_POST['submit'])){
+    $sem_name  = get_safe_value($_POST['sem_name']);
+    $eid = get_safe_value($_POST['eid']);
+    if($eid > 0){
+        mysqli_query($con,"update semesters set sem_name ='$sem_name ' where id='$eid'");
+    }else{
+        mysqli_query($con,"insert into semesters(sem_name) values('$sem_name')");
+    }
+    redirect('semester.php');
+}
+?>
                 
                  <div class="row">
                     <div class="col-12">
@@ -16,6 +35,9 @@
                                 </div>
                             </div>
                             <div class="p-4">
+                                <?php 
+                                        $res = mysqli_query($con,"select * from semesters");
+                                        if(mysqli_num_rows($res)>0){?>
                             <table class="table">
                                 <thead class="thead-dark text-white">
                                     <tr>
@@ -25,54 +47,23 @@
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    <?php
+                                            $i=1;
+                                            while($row=mysqli_fetch_assoc($res)){
+                                    ?>
                                     <tr>
-                                        <td>1</td>
-                                        <td>First Semester</td>
+                                        <td><?php echo $i ?></td>
+                                        <td><?php echo $row['sem_name']?></td>
                                         <td>
-                                            <button type="button" class="btn btn-info">Edit</button>
-                                            <button type="button" class="btn btn-danger text-white">Delete</button>
+                                            <button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal" onclick="editModal('<?php echo $row['id']?>')">Edit</button>
+                                            <a href="?did=<?php echo $row['id']?>"><button type="button" class="btn btn-danger text-white">Delete</button></a>
                                         </td>
                                     </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Second Semester</td>
-                                        <td>
-                                            <button type="button" class="btn btn-info">Edit</button>
-                                            <button type="button" class="btn btn-danger text-white">Delete</button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>Third Semester</td>
-                                        <td>
-                                            <button type="button" class="btn btn-info">Edit</button>
-                                            <button type="button" class="btn btn-danger text-white">Delete</button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>4</td>
-                                        <td>Fourth Semester</td>
-                                        <td>
-                                            <button type="button" class="btn btn-info">Edit</button>
-                                            <button type="button" class="btn btn-danger text-white">Delete</button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>5</td>
-                                        <td>Fifth Semester</td>
-                                        <td>
-                                            <button type="button" class="btn btn-info">Edit</button>
-                                            <button type="button" class="btn btn-danger text-white">Delete</button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>6</td>
-                                        <td>Sixth Semester</td>
-                                        <td>
-                                            <button type="button" class="btn btn-info">Edit</button>
-                                            <button type="button" class="btn btn-danger text-white">Delete</button>
-                                        </td>
-                                    </tr>
+                                    <?php $i++;
+                            }
+                                    }else{ ?>
+                                    <h2>No Data Found</h2>
+                                 <?php } ?>
                                 </tbody>
                             </table>
                            </div> 
@@ -81,22 +72,42 @@
                     <!-- Modal -->
                         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                           <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Semester Name</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                                </button>
-                              </div>
-                              <div class="modal-body">
-                                <input type="text" class="form-control fwb" id="sem_name" placeholder="Enter Semester Name" required="required">
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                              </div>
-                            </div>
+                            <form method="POST">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Semester Name</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                    </button>
+                                  </div>
+                                  <div class="modal-body">
+                                    <input type="text" class="form-control fwb" name="sem_name" placeholder="Enter Semester Name" required="required" id="sem_name">
+                                  </div>
+                                    <input type="hidden" name="eid" id="eid">
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <input type="submit" class="btn btn-primary" name="submit" value="Save">
+                                  </div>
+                                </div>
+                            </form>
                           </div>
                         </div>
                      <!-- Modal -->
+
+    <script type="text/javascript">
+        function editModal(id){
+            var eid = id;
+            $.ajax({
+                url : "editSemData.php",
+                type : "POST",
+                data : "editID= "+eid,
+                success : function(result){
+                    var data = jQuery.parseJSON(result);
+                    //console.log(data.dept_name);
+                    $("#sem_name").val(data.sem_name);
+                    $("#eid").val(data.id);
+                }
+            });
+        }
+    </script>                  
 <?php include('footer.php'); ?>
