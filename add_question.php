e9e3d0a64796300144d222a538c7faff80fa2d66
MahamudulHossain <md.mahamudulhossainn@gmail.com>
2021-09-15 18:55:32 +0600

    Question Addition setup(1)

--- add_question.php
+++ add_question.php
@@ -11,18 +11,14 @@
                 </div>
                 <div class="x_content">
                   <br>
-                  <form id="demo-form2" data-parsley-validate="" class="form-horizontal form-label-left" novalidate="">
-
+                  <form action="" method="POST">
                     <div class="item form-group">
                         <div class="col-md-6">
                           <label class="col-form-label col-md-3 col-sm-3 label-align">Department<span class="required">*</span>
                           </label>
                           <div class="col-md-9 col-sm-9 ">
-                          <select class="form-control">
-                            <option>Choose Department</option>
-                            <option>Computer Science and Engineering</option>
-                            <option>Mathematics</option>
-                            <option>Chemistry</option>
+                          <select class="form-control" name="dept_id" required="required" id="dept_id">
+                            <option value="">Choose Department</option>
                           </select>
                           </div>
                         </div>
@@ -30,10 +26,8 @@
                           <label class="col-form-label col-md-3 col-sm-3 label-align">Semester<span class="required">*</span>
                           </label>
                           <div class="col-md-9 col-sm-9 ">
-                          <select class="form-control">
-                            <option>Choose Semester</option>
-                            <option>First</option>
-                            <option>Second</option>
+                          <select class="form-control" name="sem_id" required="required" id="sem_id">
+
                           </select>
                           </div>
                         </div>
@@ -43,10 +37,8 @@
                           <label class="col-form-label col-md-3 col-sm-3 label-align">Subject<span class="required">*</span>
                           </label>
                           <div class="col-md-9 col-sm-9 ">
-                          <select class="form-control">
-                            <option>Choose Subject</option>
-                            <option>Data Structure</option>
-                            <option>Discrete Mathematics</option>
+                          <select class="form-control" name="sub_id" required="required" id="sub_id">
+
                           </select>
                           </div>
                         </div>
@@ -54,10 +46,8 @@
                           <label class="col-form-label col-md-3 col-sm-3 label-align">Chapter<span class="required">*</span>
                           </label>
                           <div class="col-md-9 col-sm-9 ">
-                          <select class="form-control">
-                            <option>Choose Chapter</option>
-                            <option>Introduction</option>
-                            <option>Divide and Conquer</option>
+                          <select class="form-control" name="chap_id" required="required" id="chap_id">
+
                           </select>
                           </div>
                         </div>
@@ -65,115 +55,66 @@
                           <label class="col-form-label col-md-3 col-sm-3 label-align">Level<span class="required">*</span>
                           </label>
                           <div class="col-md-9 col-sm-9 ">
-                          <select class="form-control">
-                            <option>Choose Level</option>
-                            <option>Easy</option>
-                            <option>Medium</option>
-                            <option>Hard</option>
+                            <?php $res = mysqli_query($con, "select * from levels");?>
+                          <select class="form-control" name="lvl_id" required="required" id="lvl_id">
+                              <option value="">Select Level</option>
+                            <?php while($lvlRow = mysqli_fetch_assoc($res)){ ?>
+                              <option value="<?php echo $lvlRow['id']?>"><?php echo $lvlRow['lvl']?></option>
+                            <?php } ?>
                           </select>
                           </div>
                         </div>
                     </div>
-                    
-                    <div class="col-md-12 col-sm-12 ">
-                        <div class="x_panel">
-                          <div class="x_title">
-                            <h2>Question</h2>
-                            <ul class="nav navbar-right panel_toolbox">
-                              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                              </li>
-                              <li><a class="close-link"><i class="fa fa-close"></i></a>
-                              </li>
-                            </ul>
-                            <div class="clearfix"></div>
-                          </div>
-                          <div class="x_content">
-                            <div id="alerts"></div>
-                            <div class="btn-toolbar editor" data-role="editor-toolbar" data-target="#editor-one">
-                              <div class="btn-group">
-                                <a class="btn dropdown-toggle" data-toggle="dropdown" title="Font"><i class="fa fa-font"></i><b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                </ul>
-                              </div>
 
-                              <div class="btn-group">
-                                <a class="btn dropdown-toggle" data-toggle="dropdown" title="Font Size"><i class="fa fa-text-height"></i>&nbsp;<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                  <li>
-                                    <a data-edit="fontSize 5">
-                                      <p style="font-size:17px">Huge</p>
-                                    </a>
-                                  </li>
-                                  <li>
-                                    <a data-edit="fontSize 3">
-                                      <p style="font-size:14px">Normal</p>
-                                    </a>
-                                  </li>
-                                  <li>
-                                    <a data-edit="fontSize 1">
-                                      <p style="font-size:11px">Small</p>
-                                    </a>
-                                  </li>
-                                </ul>
-                              </div>
-
-                              <div class="btn-group">
-                                <a class="btn" data-edit="bold" title="Bold (Ctrl/Cmd+B)"><i class="fa fa-bold"></i></a>
-                                <a class="btn" data-edit="italic" title="Italic (Ctrl/Cmd+I)"><i class="fa fa-italic"></i></a>
-                                <a class="btn" data-edit="strikethrough" title="Strikethrough"><i class="fa fa-strikethrough"></i></a>
-                                <a class="btn" data-edit="underline" title="Underline (Ctrl/Cmd+U)"><i class="fa fa-underline"></i></a>
-                              </div>
-
-                              <div class="btn-group">
-                                <a class="btn" data-edit="insertunorderedlist" title="Bullet list"><i class="fa fa-list-ul"></i></a>
-                                <a class="btn" data-edit="insertorderedlist" title="Number list"><i class="fa fa-list-ol"></i></a>
-                                <a class="btn" data-edit="outdent" title="Reduce indent (Shift+Tab)"><i class="fa fa-dedent"></i></a>
-                                <a class="btn" data-edit="indent" title="Indent (Tab)"><i class="fa fa-indent"></i></a>
-                              </div>
-
-                              <div class="btn-group">
-                                <a class="btn" data-edit="justifyleft" title="Align Left (Ctrl/Cmd+L)"><i class="fa fa-align-left"></i></a>
-                                <a class="btn" data-edit="justifycenter" title="Center (Ctrl/Cmd+E)"><i class="fa fa-align-center"></i></a>
-                                <a class="btn" data-edit="justifyright" title="Align Right (Ctrl/Cmd+R)"><i class="fa fa-align-right"></i></a>
-                                <a class="btn" data-edit="justifyfull" title="Justify (Ctrl/Cmd+J)"><i class="fa fa-align-justify"></i></a>
-                              </div>
-
-                              <div class="btn-group">
-                                <a class="btn dropdown-toggle" data-toggle="dropdown" title="Hyperlink"><i class="fa fa-link"></i></a>
-                                <div class="dropdown-menu input-append">
-                                  <input class="span2" placeholder="URL" type="text" data-edit="createLink">
-                                  <button class="btn" type="button">Add</button>
-                                </div>
-                                <a class="btn" data-edit="unlink" title="Remove Hyperlink"><i class="fa fa-cut"></i></a>
-                              </div>
-
-                              <div class="btn-group">
-                                <a class="btn" title="Insert picture (or just drag &amp; drop)" id="pictureBtn"><i class="fa fa-picture-o"></i></a>
-                                <input type="file" data-role="magic-overlay" data-target="#pictureBtn" data-edit="insertImage">
-                              </div>
-
-                              <div class="btn-group">
-                                <a class="btn" data-edit="undo" title="Undo (Ctrl/Cmd+Z)"><i class="fa fa-undo"></i></a>
-                                <a class="btn" data-edit="redo" title="Redo (Ctrl/Cmd+Y)"><i class="fa fa-repeat"></i></a>
-                              </div>
-                            </div>
-
-                            <div id="editor-one" class="editor-wrapper placeholderText" contenteditable="true"></div>
-
-                            <textarea name="descr" id="descr" style="display:none;"></textarea>
-
-                            <br>
-                          </div>
-                        </div>
-                      </div>
-
-                     <div class="text-right">
-                      <button class="btn btn-success btn-lg">Submit</button>
+                    <div class="text-right">
+                      <input type="submit" class="btn btn-success btn-lg" name="submit" value="Save">
                     </div>
                   </form> 
                 </div>
                   
                 </div>
               </div>
+
+    <script type="text/javascript">
+
+  
+    function loadQuestionData(type, deptID, semID, subID){
+      $.ajax({
+        url : "question_add.php",
+        type : "POST",
+        data : {type : type, deptID : deptID, semID : semID, subID : subID},
+        success : function(result){
+          if(type == "semester"){
+            $("#sem_id").html(result);
+          }else if(type == "subject"){
+            $("#sub_id").html(result);
+          }else if(type == "chapter"){
+            $("#chap_id").html(result);
+          }else{
+            $("#dept_id").append(result);
+          }
+        }
+      });
+    }
+
+  loadQuestionData();
+
+  $("#dept_id").on("change",function(){
+    loadQuestionData("semester");
+  });
+  
+  $("#sem_id").on("change",function(){
+    var deptID = $("#dept_id").val();
+    var semID = $("#sem_id").val();
+    loadQuestionData("subject",deptID,semID);
+  });
+
+  $("#sub_id").on("change",function(){
+    var deptID = $("#dept_id").val();
+    var semID = $("#sem_id").val();
+    var subID = $("#sub_id").val();
+    loadQuestionData("chapter",deptID,semID,subID);
+  });
+</script>          
 
   <?php include('footer.php');?>
