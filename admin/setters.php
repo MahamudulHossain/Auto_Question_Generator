03b0f8c36ae36edb50ce1876b4d6764470b8e76f
MahamudulHossain <md.mahamudulhossainn@gmail.com>
2021-09-20 23:13:22 +0600

    Dynamic Setter Page Complete

--- admin/setters.php
+++ admin/setters.php
@@ -1,4 +1,20 @@
-<?php include('top.php'); ?>
+<?php 
+    include('top.php');
+    if(isset($_GET['did']) && $_GET['did'] > 0){
+        $delete_id = get_safe_value($_GET['did']);
+        mysqli_query($con,"Delete from users where id='{$delete_id}'");
+        redirect('setters.php');
+    }
+    if(isset($_GET['stat'])){
+        $eid = get_safe_value($_GET['eid']);
+        if($_GET['stat'] == 0){
+            mysqli_query($con,"update users set status='0' where id='{$eid}' ");
+        }else if($_GET['stat'] == 1){
+            mysqli_query($con,"update users set status='1' where id='{$eid}' ");
+        }
+        redirect('setters.php');
+    }
+ ?>
 
                  <div class="row">
                     <div class="col-12">
@@ -12,85 +28,42 @@
                             </div>
                             <div class="p-4">
                             <table class="table">
+                               <?php $res = mysqli_query($con,"select * from users where user_role='setter' ");
+                                if(mysqli_num_rows($res)){
+                               ?> 
                                 <thead class="thead-dark text-white">
                                     <tr>
                                         <th scope="col">SL</th>
                                         <th scope="col">Name</th>
+                                        <th scope="col">Email</th>
                                         <th scope="col">Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
+                                <?php 
+                                    $i=1;
+                                    while($row = mysqli_fetch_assoc($res)){
+                                    ?>
                                     <tr>
-                                        <td>1</td>
-                                        <td>Anisur Rahman</td>
+                                        <td><?php echo $i++?></td>
+                                        <td><?php echo $row['user_name']?></td>
+                                        <td><?php echo $row['user_email']?></td>
                                         <td>
-                                            <button type="button" class="btn btn-info"data-toggle="modal" data-target="#exampleModal">Details</button>
-                                            <button type="button" class="btn btn-success">Active</button>
-                                            <button type="button" class="btn btn-danger text-white">Delete</button>
+                                            <?php if($row['status'] == 0){?>
+                                                <a href="?stat=1&eid=<?php echo $row['id']?>"><button type="button" class="btn btn-warning">Deactive</button></a>
+                                            <?php }else{?>
+                                                 <a href="?stat=0&eid=<?php echo $row['id']?>"><button type="button" class="btn btn-success">Active</button></a>
+                                            <?php } ?>    
+                                            <a href="?did=<?php echo $row['id']?>"><button type="button" class="btn btn-danger text-white">Delete</button></a>
                                         </td>
                                     </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Mizan</td>
-                                        <td>
-                                            <button type="button" class="btn btn-info">Details</button>
-                                            <button type="button" class="btn btn-warning">Deactive</button>
-                                            <button type="button" class="btn btn-danger text-white">Delete</button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>Sharif Ahmad</td>
-                                        <td>
-                                            <button type="button" class="btn btn-info">Details</button>
-                                            <button type="button" class="btn btn-success">Active</button>
-                                            <button type="button" class="btn btn-danger text-white">Delete</button>
-                                        </td>
-                                    </tr>
+                                <?php } ?>
                                 </tbody>
+                            <?php }else{?>
+                                <h3>No Data Found</h3>
+                            <?php } ?>
                             </table>
                            </div> 
                         </div> 
 
-                    <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                          <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Question Setter Details</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                                </button>
-                              </div>
-                              <div class="modal-body">
-                                <table class="table">
-                                    <tr>
-                                        <td>Name</td>
-                                        <td>Anisur Rahman</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Deapartment</td>
-                                        <td>Computer Science and Engineering</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Subject</td>
-                                        <td>Data Structure</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Email</td>
-                                        <td>anisur@gmail.com</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Mobile</td>
-                                        <td>12345678912</td>
-                                    </tr>
-                                </table>
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                     <!-- Modal -->
 <?php include('footer.php'); ?>
